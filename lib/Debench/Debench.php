481a800e029bfe92673441c6eb79b9b1098b3237
Mohammad Yaaghubi <m.yaghobi.abc@gmail.com>
2024-03-29 21:04:35 +0330

    Some getters

--- lib/Debench/Debench.php
+++ lib/Debench/Debench.php
@@ -10,14 +10,76 @@
 
 class Debench
 {
+    private string $base;
+
+    private mixed $checkPoints;
+    private int $ramUsageMax;
+    
+    private int $lastCheckPointInMS;
+    private int $lastCheckPointNumber;
+
     /**
-     * Debench constructor
+     * Debench Constructor
      *
      * @return void
      */
-    function __construct()
+    function __construct(private bool $active=true)
     {
+        if (!$this->active) {
+            return;
+        }
+
         $backtrace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
         $this->base = dirname(($backtrace[0])['file']);
+
+        // CheckPoints[]
+        $this->checkPoints = [];
+        $this->ramUsageMax = 0;
+        $this->lastCheckPointInMS = 0;
+        $this->lastCheckPointNumber = 0;
+    }
+
+
+    /**
+     * Is Debench enable
+     *
+     * @return bool
+     */
+    public function isEnable(): bool
+    {
+        return $this->active;
+    }
+
+
+    /**
+     * Get the last checkpoint in milliseconds
+     *
+     * @return int
+     */
+    public function getLastCheckPointInMS(): int
+    {
+        return $this->lastCheckPointInMS;
+    }
+
+
+    /**
+     * Get the last checkpoint number
+     *
+     * @return int
+     */
+    public function getLastCheckPointNumber(): int
+    {
+        return $this->lastCheckPointNumber;
+    }
+
+
+    /**
+     * Get checkpoints array
+     *
+     * @return array<string,object>
+     */
+    public function getCheckPoints(): mixed
+    {
+        return $this->checkPoints;
     }
 }
